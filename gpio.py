0a9b9062bfde3634bf9a20f4a302976189e462f9
Thiago Henrique <39107746+Thhc10@users.noreply.github.com>
2020-08-11 11:56:47 -0300

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -6,22 +6,29 @@ import threading
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-#GPIO.setup(26 , GPIO.OUT)
+
 GPIO.setup(20 , GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Recebe as oscilacoes
 GPIO.setup(19 , GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Reseta
 GPIO.setup(16 , GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Total voltas
 
 espeak.set_voice('brazil')
 espeak.set_parameter(espeak.Parameter.Rate, 175, 0)
-#pwm = GPIO.PWM(26, 1) # Canal, Frequencia
-#pwm.start(50)         # Duty cicle
+GPIO.setup(26 , GPIO.OUT)
+
+'''
+Simulação com o PWM
+
+pwm = GPIO.PWM(26, 1) # Canal, Frequencia
+pwm.start(50)         # Duty cicle
+pwm.ChangeFrequency(nova_freq)
+pwm.ChangeDutyCycle(novo_dc)
+pwm.stop()
+'''
+
 espeak.synth("Vamos começar")
 
 cont = 0
 cont_total = 0
-# pwm.ChangeFrequency(nova_freq)
-# pwm.ChangeDutyCycle(novo_dc)
-# pwm.stop()
 
 def frequencia():
     global cont, cont_total
@@ -34,7 +41,11 @@ def frequencia():
     cont = cont + 2
     cont_total = cont_total + 2
     return freq
+
 '''
+Retirada da fala de velocidade, para
+não ficar confuso
+
 class x_rpm(Thread):
     def __init__(self, event):
         Thread.__init__(self)
@@ -54,7 +65,6 @@ class x_rpm(Thread):
             espeak.set_parameter(espeak.Parameter.Rate, 375, 0)
             
 '''
-#
 
 def reset_save():
     global cont
@@ -74,15 +84,15 @@ def get_rot_total():
             espeak.synth("Foram dadas" + str(cont_total) + "voltas.")
             espeak.set_parameter(espeak.Parameter.Rate, 150, 0)
             time.sleep(0.5)
-            
-            
+             
 # Se GPIO(19) acionado -> Reseta e Salva o intervalo
 
 t1 = threading.Thread(target=reset_save)
 t1.start()
 
-# Retorna a RPM relativa a cada 15seg
 '''
+Retorna a RPM relativa a cada 15seg
+
 stopFlag1 = Event()
 get_rpm = x_rpm(stopFlag1)
 get_rpm.start()
@@ -94,9 +104,6 @@ t2.start()
 
 freq = frequencia();
 
-print(freq)
-freq = 1
-
 while(freq < 1500):    
        
     if(freq >= 500):
@@ -106,7 +113,6 @@ while(freq < 1500):
         
         if(cont % 500 == 0):
             espeak.synth(str(cont))
-            
         
     elif(freq  >= 200):
             GPIO.wait_for_edge(20, GPIO.RISING)
@@ -115,7 +121,6 @@ while(freq < 1500):
             
             if(cont % 200 == 0):
                 espeak.synth(str(cont))
-                
         
     elif(freq >= 75):
             GPIO.wait_for_edge(20, GPIO.RISING)
@@ -124,7 +129,6 @@ while(freq < 1500):
             
             if(cont % 75 == 0):
                 espeak.synth(str(cont))
-                
                    
     elif(freq >= 25):
             GPIO.wait_for_edge(20, GPIO.RISING)
@@ -133,7 +137,6 @@ while(freq < 1500):
             
             if(cont % 25 == 0):
                 espeak.synth(str(cont))
-                
 
     elif(freq >= 10):
             GPIO.wait_for_edge(20, GPIO.RISING)
@@ -143,7 +146,6 @@ while(freq < 1500):
             if(cont % 10 == 0):
                 espeak.synth(str(cont))
                 
-
     elif(freq >= 3.5):
             GPIO.wait_for_edge(20, GPIO.RISING)
             cont = cont + 1
@@ -151,16 +153,11 @@ while(freq < 1500):
             
             if(cont % 5 == 0):
                 espeak.synth(str(cont))
-            
-                   
+                  
     else:
             espeak.synth(str(cont))
             GPIO.wait_for_edge(20, GPIO.RISING)
             cont = cont + 1
             cont_total = cont_total + 1
-            print(cont)
-            #time.sleep(0.05)
             
-pwm.stop()
-
 exit(0)    
