ce1c920530b318dcc489f9a3f87fd6cc31c1c3f4
Thiago Henrique <39107746+Thhc10@users.noreply.github.com>
2020-08-13 23:54:48 -0300

    Contador de passos com função velocidade

--- contadorpassos.py
+++ contadorpassos.py
@@ -20,6 +20,7 @@ espeak.synth("Vamos começar")
 cont = 0
 cont_total = 0
 
+# Encontrando a frequência de rotação 
 def frequencia():
     global cont, cont_total
     GPIO.wait_for_edge(20, GPIO.RISING)
@@ -32,9 +33,7 @@ def frequencia():
     cont_total = cont_total + 2
     return freq
 
-'''
-# Retirada da fala de velocidade, para não ficar confuso
-
+# Encontrando a velocidade
 class x_rpm(Thread):
     def __init__(self, event):
         Thread.__init__(self)
@@ -52,36 +51,34 @@ class x_rpm(Thread):
             espeak.set_parameter(espeak.Parameter.Rate, 150, 0)
             espeak.synth("Sua RPM atual é de:" + str(int(rpm)))
             espeak.set_parameter(espeak.Parameter.Rate, 375, 0) 
-'''
 
+# Resetando a contagem
 def reset_save():
     global cont
     while(1):
-        if(GPIO.input(19) == 1): # Fazer verificar uma borda
+        if(GPIO.input(19) == 1): # Verificação de borda
             cont = 0
             time.sleep(0.5)
 
+# Retornando a contagem total de passos
 def get_rot_total():
     global cont_total
     while(1):
-        if(GPIO.input(16) == 1):  # Fazer verificar uma borda
+        if(GPIO.input(16) == 1):  # Verificação de borda
             espeak.set_parameter(espeak.Parameter.Rate, 150, 0)
             espeak.synth("Foram dadas" + str(cont_total) + "voltas.")
             espeak.set_parameter(espeak.Parameter.Rate, 150, 0)
             time.sleep(0.5)
              
 # Se GPIO(19) acionado -> Reseta e Salva o intervalo
-
 t1 = threading.Thread(target=reset_save)
 t1.start()
 
-'''
 # Retorna a RPM relativa a cada 15seg
-
 stopFlag1 = Event()
 get_rpm = x_rpm(stopFlag1)
 get_rpm.start()
-'''
+
 
 # Se GPIO(16) acionado -> Retorna a distancia total
 t2 = threading.Thread(target=get_rot_total)
@@ -89,6 +86,7 @@ t2.start()
 
 freq = frequencia();
 
+# Definindo conjuntos de frequências e retornando em áudio
 while(freq < 1500):    
        
     if(freq >= 500):
@@ -97,6 +95,7 @@ while(freq < 1500):
         cont_total = cont_total + 1
         
         if(cont % 500 == 0):
+            # Retornando a contagem em áudio
             espeak.synth(str(cont))
         
     elif(freq  >= 200):
